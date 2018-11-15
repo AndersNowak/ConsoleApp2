8b001a5493af6a994c5350c3c39f57ac3f5c26ed
pxdnowak <36161994+pxdnowak@users.noreply.github.com>
2018-11-15 11:05:47 +0100

    erik snakker hurtigt

---
+++ ConsoleApp2/Erik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Erik
+    {
+        
+
+        public string Navn { get; }
+        public int Alder { get; }
+
+        public Erik(string navn, int alder, bool talerHurtigt)
+        {
+            Navn = navn;
+            Alder = alder;
+            
+        }
+    }
+}
